86144bf2297b22499b346806cae15589ccfb5342
Yash Gangwar <gangwaryash06@gmail.com>
2021-09-21 01:56:09 +0530

    env file fixes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7AxXcV5Qdv6adoAyKNCQ0HythgAWMDTg",
-    authDomain: "assignment-submission-form.firebaseapp.com",
-    projectId: "assignment-submission-form",
-    storageBucket: "assignment-submission-form.appspot.com",
-    messagingSenderId: "755813947907",
-    appId: "1:755813947907:web:db6350d39c0f769bc68c36"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 
